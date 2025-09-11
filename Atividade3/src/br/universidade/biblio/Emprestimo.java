38b76acdbe5a371fa0cb8d3fd9b96e8b2c50dada
BlackbirdBlina <sabrinapop_rn@hotmail.com>
2025-09-11 20:38:56 -0300

    Adicionando novo atributo à classe

--- Atividade3/src/br/universidade/biblio/Emprestimo.java
+++ Atividade3/src/br/universidade/biblio/Emprestimo.java
@@ -6,12 +6,14 @@ import java.time.ZoneId;
 import java.util.Date;
 
 public class Emprestimo {
+    private int id;
     private Date dataEmprestimo;
     private Date dataDevolucao;
     private Livro livro;
     private Usuario usuario;
 
-    public Emprestimo(LocalDate dataEmprestimo, LocalDate dataDevolucao, Livro livro, Usuario usuario) {
+    public Emprestimo(int id, LocalDate dataEmprestimo, LocalDate dataDevolucao, Livro livro, Usuario usuario) {
+        this.id = id;
         this.dataEmprestimo = Date.from(dataEmprestimo.atStartOfDay(ZoneId.systemDefault()).toInstant());
         this.dataDevolucao = Date.from(dataDevolucao.atStartOfDay(ZoneId.systemDefault()).toInstant());
         this.livro = livro;
@@ -20,6 +22,7 @@ public class Emprestimo {
 
     public Date getDataEmprestimo() { return dataEmprestimo; }
     public Date getDataDevolucao() { return dataDevolucao; }
+    public int getId() { return id; }
     public Livro getLivro() { return livro; }
     public Usuario getUsuario() { return usuario; }
 
