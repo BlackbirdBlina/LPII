3e77f8d535c9413d5889b0fd805e6bec26bbfaee
BlackbirdBlina <sabrinapop_rn@hotmail.com>
2025-09-17 21:17:04 -0300

    Removendo informações desnecessárias da classe Pagamento, deixando-a como interface

--- Atividade4/src/br/universidade/ecommerce/Pagamento.java
+++ Atividade4/src/br/universidade/ecommerce/Pagamento.java
@@ -1,15 +1,5 @@
 package br.universidade.ecommerce;
 
-import java.util.ArrayList;
-import java.util.List;
-
 abstract class Pagamento {
-    private double valorTotal;
-    private List<Produto> produtos;
-
-    Pagamento(){
-        this.produtos = new ArrayList<>();
-    }
     public abstract void processar(double valorTotal);
-
 }
