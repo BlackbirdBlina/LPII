59bf6c0ca3b9b7cec4853dc5eef0480d1fbaee62
BlackbirdBlina <sabrinapop_rn@hotmail.com>
2025-09-18 10:54:16 -0300

    Adicionando validação mínima para atributos valor e peso, bem como comentário acerca do construtor de Produto

--- Atividade4/src/br/universidade/ecommerce/Produto.java
+++ Atividade4/src/br/universidade/ecommerce/Produto.java
@@ -6,7 +6,13 @@ public class Produto {
     private double valor;
     private double peso;
 
+    /* Não sei se entendi direito o que foi pedido em relação ao Overload, mas criei um construtor
+    sem quantidade, já que a quantidade será definida no Pedido (sobrecarga adicionarItem). Conforme consta no 2º ponto (Overload) */
     public Produto(String codProduto, double valor, double peso) {
+        // Validação simples para evitar valores negativos conforme último ponto (Validação)
+        if(valor < 0 || peso < 0){
+            throw new IllegalArgumentException("Valor e peso devem ser maiores ou iguais a zero.");
+        }
         this.codProduto = codProduto;
         this.valor = valor;
         this.peso = peso;
@@ -39,7 +45,7 @@ public class Produto {
     public double getPeso() {
         return peso;
     }
-    
+
     public void setPeso(double peso) {
         this.peso = peso;
     }
